fddecbe52e066855358da76c50eac9c22fb2317a
Rob Scherer <robscherer6@gmail.com>
2022-12-10 13:49:28 -0500

    pausing here, attempting to deepen understanding

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = querySelector('.main-content');
+
+function pageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener('click', () => {
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].className = currentBtn[0].className.replace
+    })
+  }
+}
+
+
