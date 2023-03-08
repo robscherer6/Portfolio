0996b9fba591c8c082f90bd7bf787f62e025d9a8
Rob Scherer <robscherer6@gmail.com>
2023-03-08 17:12:53 -0500

    portfolio mostly complete, google sheet access implemented via api request

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var tablinks = document.getElementsByClassName('tab-links');
 var tabcontents = document.getElementsByClassName('tab-contents');
 var sideMenu = document.querySelector('.sideMenu');
+var form = document.querySelector('form');
+let msg = document.querySelector('.msg');
+console.log(msg);
 
 function opentab(tabname) {
   for (tablink of tablinks) {
@@ -20,4 +23,20 @@ function openMenu () {
 function closeMenu () {
   sideMenu.style.right = '-200px';
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyBe0eWiZK0UGw9WyrK2kT04tMJpcUI_njHd9HC38fPw17FUKWKYKXzf-vrgych4bsK/exec'
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = 'Message sent!';
+      setTimeout(function() {
+        message.innerHTML = '';
+      }, 5000)
+      console.log('Success!', response)
+      form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
 
